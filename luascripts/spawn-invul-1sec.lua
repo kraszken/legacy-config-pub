5e6c1cb40e7a4b4579554616cbdf9c56ad6368cb
Oksii <oxymoron.et@gmail.com>
2024-08-10 12:45:29 +0200

    Update spawn-invul-1sec.lua

--- luascripts/spawn-invul-1sec.lua
+++ luascripts/spawn-invul-1sec.lua
@@ -1,22 +1,10 @@
 local modname = "spawn-invul-1sec"
-local version = "0.1"
-
-local function removeSpawnShield(clientNum)
-    et.gentity_set(clientNum, "ps.powerups", et.PW_INVULNERABLE, 0)
-end
+local version = "0.2"
 
 function et_ClientSpawn(clientNum, revived)
-    if not revived then
-        et.gentity_set(clientNum, "ps.powerups", et.PW_INVULNERABLE, 1)
-        et.G_Timer(1000, function()
-            removeSpawnShield(clientNum)
-        end)
-    else
-        et.gentity_set(clientNum, "ps.powerups", et.PW_INVULNERABLE, 1)
-        et.G_Timer(1000, function()
-            removeSpawnShield(clientNum)
-        end)
-    end
+    -- Set the spawnshield duration to 1 second
+    local newDuration = et.trap_Milliseconds() + 1000
+    et.gentity_set(clientNum, "ps.powerups", et.PW_INVULNERABLE, newDuration)
 end
 
 function et_InitGame()
