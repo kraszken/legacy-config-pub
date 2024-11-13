45f17f53977365d99ea54fe91942a3823c56a544
oksii <oksii@duck.com>
2024-11-13 16:07:47 +0100

    Update/add commandmaps to public repo. Update/add spawn-invul-1sec.lua to public repo.

--- luascripts/spawn-invul-1sec.lua
+++ luascripts/spawn-invul-1sec.lua
@@ -1,12 +1,31 @@
-local modname = "spawn-invul-1sec"
-local version = "0.2"
+spawnShield = 1 -- seconds
+levelTime = 0
+hs_maps = {"purefrag", "et_headshot2_b2", "mp_sillyctf", "ctf_multi", "multi_huntplace", "badplace4_rc", "te_valhalla"}
+flag = false
 
-function et_ClientSpawn(clientNum, revived)
-    -- Set the spawnshield duration to 1 second
-    local newDuration = et.trap_Milliseconds() + 1000
-    et.gentity_set(clientNum, "ps.powerups", et.PW_INVULNERABLE, newDuration)
+function has_value (tab, val)
+	for index, value in ipairs(tab) do
+		if value == val then
+			return true
+		end
+	end
+	return false
 end
 
-function et_InitGame()
-    et.RegisterModname(modname .. " " .. version)
+function et_InitGame(levelTime, randomSeed, restart)
+	et.RegisterModname("spawnshield.lua "..et.FindSelf())
+	mapname = et.trap_Cvar_Get("mapname")
+	if has_value(hs_maps, string.lower(mapname)) then
+		flag = true
+	end
+end
+
+function et_RunFrame(lvltime)
+	levelTime = lvltime
+end
+
+function et_ClientSpawn(clientNum, revived)
+	if flag == true then
+		et.gentity_set(clientNum, "ps.powerups", 1, levelTime + spawnShield * 1000)
+	end
 end
